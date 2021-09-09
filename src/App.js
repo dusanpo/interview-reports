0f18e4e20911f3617c81559059ed01fd0d8d2fd6
vladimir-799 <vladimir.bognar.dev@gmail.com>
2021-09-09 20:05:38 +0200

    dummy page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React ,{Fragment} from 'react';
 
+
 import './App.css';
+
 import LoginPage from './components/LoginPage';
+import LandingPage from './components/LandingPage';
 
 function App() {
   return (
    <Fragment>
       <LoginPage />
-      </Fragment>
+      <LandingPage/>
+    </Fragment>
    
   );
 }
---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './partials/Header'
+import Main from './Main'
+import Footer from './partials/Footer'
+
+const LandingPage = () => {
+    return (
+        <div>
+            <Header/>
+            <Main/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default LandingPage
