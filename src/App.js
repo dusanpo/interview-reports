c3f2bb03d57a832069b22f07956325a4245a0cd2
AcaPetrovic54 <aca.t.petrovic@gmail.com>
2021-09-10 17:09:13 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-
+import { Fragment } from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <Fragment>
+      <div className="App">
+        <h1>Hello</h1>
+      </div>
+    </Fragment>
+    
   );
 }
 
