ccb4d0f54d760f95582b1a3cbd1eddb3e3915b1c
Dusan Spasic <44261977+dusanpo@users.noreply.github.com>
2021-09-11 10:04:58 +0200

    updating landing page with list of candidates

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 
-const Main = () => {
-    return (
-        <div>
-            This is main page
-        </div>
-    )
-}
+const Main = ({items}) =>{
 
+  
+       
+        return(
+
+            <div>
+            {items.map(item =>(
+            <div key = {item.id}>
+              {item.name} - {item.email} - {item.education}
+            </div>
+ 
+            ))}
+ 
+        </div>
+
+        )
+  
+    
+
+    
+}
 export default Main
