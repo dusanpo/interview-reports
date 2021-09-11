12b5da95336299ae2f045270ada3b0f4eaafe216
AcaPetrovic54 <aca.t.petrovic@gmail.com>
2021-09-11 12:56:22 +0200

    Added router

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React ,{Fragment} from 'react';
+
+
+// import LoginPage from './components/LoginPage';
+// import LandingPage from './components/LandingPage';
+
+const Home = () => {
+  return (
+   <Fragment>
+      {/* <LoginPage />
+      <LandingPage/> */}
+    </Fragment>
+   
+  );
+}
+
+export default Home;
