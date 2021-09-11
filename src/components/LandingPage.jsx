05df930e620cd4a7e36526aa26e7dce1be9232a6
vladimir-799 <vladimir.bognar.dev@gmail.com>
2021-09-11 10:58:50 +0200

    error solved

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,50 +1,58 @@
-import React from 'react'
-import Header from './partials/Header'
-import Main from './Main'
-import Footer from './partials/Footer'
+import React from "react";
+import Header from "./partials/Header";
+import Main from "./Main";
+import Footer from "./partials/Footer";
 
-class LandingPage extends React.Component{
-    constructor(props){
-      super(props);
-      this.state = {
-        items: [],
-        isLoaded: false
-      }
-    }
-    
-  componentDidMount(){
+class LandingPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    };
+  }
+
+  componentDidMount() {
     const USER_ACCESS_TOKEN = JSON.parse(localStorage.getItem("token"));
-    fetch("http://localhost:3333/api/candidates",{
+    if(USER_ACCESS_TOKEN) {
+    fetch("http://localhost:3333/api/candidates", {
       method: "GET",
       headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + USER_ACCESS_TOKEN
-      }
-  
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + USER_ACCESS_TOKEN,
+      },
     })
-   
-    .then(res => res.json())
-    .then(data => {
-      console.log(data);
-        this.setState({
-          isLoaded: true,
-          items: data
-        })
-    });
+      .then(
+        (res) => res.json(),
+        () => {
+          localStorage.removeItem("token");
+          return;
+        }
+      )
+      .then((data) => {
+        console.log(data);
+        if (data && data.length > 0) {
+          this.setState({
+            isLoaded: true,
+            items: data,
+          });
+        }
+      });
+    }
   }
-     
-    render(){
-const{isLoaded} = this.state;
-   if(!isLoaded){
-     return <div>Loading</div>
-   }
+
+  render() {
+   
+    if (!this.state.isLoaded) {
+      return <div>Loading</div>;
+    }
     return (
-        <div>
-            <Header/>
-            <Main items = {this.state.items}/>
-            <Footer/>
-        </div>
-    )
+      <div>
+        <Header />
+        <Main items={this.state.items} />
+        <Footer />
+      </div>
+    );
+  }
 }
-}
-export default LandingPage
+export default LandingPage;
