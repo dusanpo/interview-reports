5fdfc57257fa737f7dfbc06353f5980fab42d312
Oliver Graovac <olivergraovac@gmail.com>
2021-09-11 16:07:54 +0200

    Stylized main page

--- src/App.js
+++ src/App.js
@@ -1,36 +1,33 @@
-import React from 'react';
-import {Switch, Route, Redirect, useHistory} from "react-router-dom";
+import React from "react";
+import { Switch, Route, Redirect, useHistory } from "react-router-dom";
 import Header from "./components/partials/Header";
 import Home from "./components/Home";
 import LoginPage from "./components/LoginPage";
 
-import './App.css';
+import "./App.css";
 
-import LandingPage from './components/LandingPage';
+import LandingPage from "./components/LandingPage";
 
 function App() {
   let history = useHistory();
- let header = <Header/>
-  let token = localStorage.getItem("token")
-  if(!token) {
-    history.push("/loginPage");
+  let header = <Header />;
+  let token = localStorage.getItem("token");
+  if (!token) {
+    history.push("/landingPage");
     header = null;
   }
 
   return (
-   
     <div>
       {header}
-        <Switch>
-          <Route exact path= "/home" component={Home} />
-          <Route exact path= "/landingPage" component={LandingPage} />
-          <Route exact path= "/loginPage" component={LoginPage} />
-          <Route exact path="/">
-            <Redirect to="/loginPage"/>
-          </Route>
-          
-          
-        </Switch>
+      <Switch>
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/landingPage" component={LandingPage} />
+        <Route exact path="/loginPage" component={LoginPage} />
+        <Route exact path="/">
+          <Redirect to="/loginPage" />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
+import React from "react";
 
-const Main = ({items}) =>{
-
-  
-       
-        return(
-
-            <div>
-            {items.map(item =>(
-            <div key = {item.id}>
-              {item.name} - {item.email} - {item.education}
+const Main = ({ items }) => {
+  return (
+    <div className="row container flexy">
+      {items.map((item, index) => (
+        <div className="col s6 m4" key={index}>
+          <div className="card">
+            <div className="card-image">
+              <img
+                src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+                alt="img"
+              />
+              <span className="card-title black-text text-darken-2">
+                {item.name}
+              </span>
             </div>
- 
-            ))}
- 
+            <div className="card-content">
+              <p>{item.education}</p>
+              <p>{item.email}</p>
+            </div>
+          </div>
         </div>
-
-        )
-  
-    
-
-    
-}
-export default Main
+      ))}
+    </div>
+  );
+};
+export default Main;
